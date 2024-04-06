1de50b35e247a6cd2f7cd0f8888bca08af7d23af
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-04-06 15:56:16 +0900

    Title: 포탑 부수기, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240406/포탑 부수기/destroy-the-turret.py
+++ 240406/포탑 부수기/destroy-the-turret.py
@@ -6,25 +6,23 @@ game = [list(map(int, input().split())) for _ in range(N)]
 count = [[0] * M for _ in range(N)]
 
 def attacker(): # 공격자 선정 함수
-    tmp = [5001, 0, 0]
-    isMe = False
+    tmp = [5001, 0, 0, 1001]
     for y in range(N):
         for x in range(M):
             # 공격력이 가장 낮은.
-            if tmp[0] > game[y][x] and game[y][x]:
-                if [y, x] == tmp[1:]:
-                    isMe = True
-                tmp = [game[y][x], y, x]
-            # 자신이 선택된걸 제외
-            elif tmp[0] == game[y][x] and not isMe and not game[y][x]:
-                if tmp[1:] == [y, x]:
-                    tmp[0] = game[y][x]
-                    isMe = True
-                elif sum(tmp[1:]) < (y+x):
-                    tmp = [game[y][x], y, x]
-                # 열이 높은
-                elif sum(tmp[1:]) == (y+x) and tmp[2] < x:
-                    tmp = [game[y][x], y, x]
+            if tmp[0] > game[y][x] and game[y][x] != 0:
+                tmp = [game[y][x], y, x, count[y][x]]
+            # 만약 같다면
+            elif tmp[0] == game[y][x]:
+                # 가장 최근에 공격한
+                if tmp[3] < count[y][x]:
+                    tmp = [game[y][x], y, x, count[y][x]]
+                # 둘다 공격한 적이 없다면, 열+행 큰값
+                elif tmp[3] == count[y][x] and sum(tmp[1:3]) < (y+x):
+                    tmp = [game[y][x], y, x, count[y][x]]
+                # 둘다 공격한 적이 없다면, 열+행이 같다면, 열이 높은
+                elif tmp[3] == count[y][x] and sum(tmp[1:3]) < (y+x) and tmp[2] < x:
+                    tmp = [game[y][x], y, x, count[y][x]]
     return tmp
 
 def reciver(ny, nx): # 공격자 제외 가장 강한 적을 뽑는 함수
@@ -43,7 +41,7 @@ def reciver(ny, nx): # 공격자 제외 가장 강한 적을 뽑는 함수
                     if re_tmp[3] > count[y][x]:
                         re_tmp = [game[y][x], y, x, count[y][x]]
                     # 행과 열의 합이 가장 작은
-                    elif sum(re_tmp[1:3]) > (y + x):
+                    elif re_tmp[3] == count[y][x] and sum(re_tmp[1:3]) > (y + x):
                         re_tmp = [game[y][x], y, x, count[y][x]]
                     # 열이 가장 작은
                     elif sum(re_tmp[1:3]) == (y + x) and re_tmp[2] > x:
@@ -58,13 +56,13 @@ def laser(ay, ax, ry, rx, visit):
         for i in range(4):
             ny, nx = ay + dy[i], ax + dx[i]
             if 0>ny:
-                ny = N+dy[i]
+                ny = N+ny
             elif N<=ny:
-                ny = N-dy[i]
+                ny = N-ny
             if 0>nx:
-                nx = N+dx[i]
+                nx = N+nx
             elif N<=nx:
-                nx = N-dx[i]
+                nx = N-nx
             if game[ny][nx] != 0 and not visit[ny][nx]:
                 visit[ny][nx] = 1
                 laser(ny, nx, ry, rx, visit)
@@ -73,8 +71,6 @@ def laser(ay, ax, ry, rx, visit):
                 else:
                     visit[ny][nx] = 0
     return False
-
-
 
 def attack(ay, ax, ry, rx): # 공격 함수(레이저, 포탄)
     visit = [[0] * M for _ in range(N)]
@@ -101,15 +97,15 @@ def attack(ay, ax, ry, rx): # 공격 함수(레이저, 포탄)
         for i in range(8):
             by, bx = ry + dy[i], rx + dx[i]
             if 0>by:
-                by = N+dy[i]
+                by = N+by
             elif N<=by:
-                by = N-dy[i]
+                by = N-by
             if 0>bx:
-                bx = N+dx[i]
+                bx = N+bx
             elif N<=bx:
-                bx = N-dx[i]
+                bx = N-bx
             if game[by][bx] != 0:
-                game[by][bx] -= (game[ay][ax] + N+M)
+                game[by][bx] -= game[ay][ax]//2
                 take_attack[by][bx] = 1
                 if game[by][bx] < 0:
                     game[by][bx] = 0
@@ -124,22 +120,23 @@ def strong():
         if max(line) > big:
             big = max(line)
     return big
-#
-# for i in game:
-#     print(i)
 
-
-for _ in range(K):
+for i in range(K):
     canon = attacker() # [공격력, y, x]
+    count[canon[1]][canon[2]] = i+1
+    # print("공격 캐논", canon)
     enemy = reciver(canon[1], canon[2]) # [공격력, y, x, 공격 순서]
+    # print("공격 대상", enemy)
     attack(canon[1], canon[2], enemy[1], enemy[2])
-
+    # print("---------", i+1, "차 공격 -----------")
+    # for i in game:
+    #     print(i)
+    cnt = 0
+    for i in game:
+        for j in i:
+            if j > 0:
+                cnt += 1
+    if cnt <= 1:
+        break
 answer = strong()
-# print("공격 캐논", canon)
-# print("공격 대상", enemy)
-#
-# print("공격 결과")
-# for i in game:
-#     print(i)
-
 print(answer)
