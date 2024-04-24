f49878070483b99a74c6b9ae18c2783ddbabefcf
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-04-24 15:30:51 +0900

    Title: 두 실수의 곱, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240424/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double a, b, c;
+        a = 5.26;
+        b = 8.27;
+        c = a*b;
+        System.out.printf("%.3f", c);
+    }
+}
