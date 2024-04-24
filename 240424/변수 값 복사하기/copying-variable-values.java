d59647ad0922cbd1817f465e368efcb50ef079b9
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-04-24 15:44:43 +0900

    Title: 변수 값 복사하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240424/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        a=1;
+        b=2;
+        c=3;
+        a=b=c;
+        System.out.printf("%s %s %s", a, b, c);
+    }
+}
