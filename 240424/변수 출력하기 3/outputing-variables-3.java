c8c87515764e1c7cfee20561114586ca0d9937e8
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-04-24 15:21:52 +0900

    Title: 변수 출력하기 3, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240424/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        char c;
+        a = 1;
+        b = 2;
+        c = 'C';
+        System.out.printf("%d->%d->%s", a, b, c);
+    }
+}
