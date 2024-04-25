fa53abb8e710b3cf4e003f610eaa07452e067f80
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-04-25 16:32:20 +0900

    Title: a 부터 b 까지, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240425/a 부터 b 까지/a-to-b.java
+++ 240425/a 부터 b 까지/a-to-b.java
@@ -6,11 +6,11 @@ public class Main {
         int a, b;
         a=sc.nextInt();
         b=sc.nextInt();
-        System.out.print(a+" ");
-        while(a<b){
+        while(a<=b){
+            System.out.print(a+" ");
             if (a%2==1) a*=2;
             else a+=3;
-            System.out.print(a+" ");
+            
         }
     }
 }
