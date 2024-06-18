f2712ea5528bc0357eaf0e848b89509037e5bede
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-06-18 12:28:06 +0900

    Title: 369 게임, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240618/369 게임/369-game.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=1;i<=n;i++){
+            if (i%3==0 || (i>10 && (i%10)%3==0)) {
+                System.out.print(0+" ");
+            } else {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
