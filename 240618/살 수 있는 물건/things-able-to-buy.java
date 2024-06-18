58ae0d2d4a3140fbe343d0f489775703f9b97a74
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-06-18 10:11:48 +0900

    Title: 살 수 있는 물건, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240618/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+        a=sc.nextInt();
+        
+        if (a>=3000) {
+            System.out.println("book");
+        } else if (a>=1000) {
+            System.out.println("mask");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
