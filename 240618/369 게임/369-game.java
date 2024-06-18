8556263cafec2d8ff9e5454d76d4c12fe24d956d
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-06-18 12:32:02 +0900

    Title: 369 게임, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240618/369 게임/369-game.java
+++ 240618/369 게임/369-game.java
@@ -8,7 +8,7 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i=1;i<=n;i++){
-            if (i%3==0 || (i%10!=0 && i>10 && (i%10)%3==0) ) {
+            if (i%3==0 || (i%10!=0 && i>10 && (i%10)%3==0) || (i>10 && i/10%3 ==0)) {
                 System.out.print(0+" ");
             } else {
                 System.out.print(i+" ");
